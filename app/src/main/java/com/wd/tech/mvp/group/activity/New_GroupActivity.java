ce6610cd6d1d4a0e7e3c74df9958465b8f01a592
zhaoxuechong <zhaoxuechong@Outlook.com>
2018-09-28 15:16:27 +0800

    新建群族

--- app/src/main/java/com/wd/tech/mvp/group/activity/New_GroupActivity.java
+++ app/src/main/java/com/wd/tech/mvp/group/activity/New_GroupActivity.java
@@ -20,17 +20,7 @@ public class New_GroupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new__group);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 
